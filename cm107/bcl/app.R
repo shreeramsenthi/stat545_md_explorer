4655269dd4631701d74074e47c608efa77614738
Shreeram Senthivasan <shreeramsenthi@gmail.com>
2018-11-15 10:48:59 -0800

    Added material from lecture 8

--- cm107/bcl/app.R
+++ cm107/bcl/app.R
@@ -1,16 +1,23 @@
 library(shiny)
-
-#shinyApp(ui, server)
-
+library(tidyverse)
 bcl <- readr::read_csv("bcl-data.csv")
 
 ui <- fluidPage(
   titlePanel(
     "BC Liquor Price",
-    "BCL APP (Window Title)"
+    "BCL APP"
   ),
   sidebarLayout(
-    sidebarPanel("This text goes in the side bar"),
+    sidebarPanel(
+      sliderInput("priceInput", "Select your desired price range.",
+        min = 0, max = 100, value = c(15, 30), pre = "$"
+      ),
+      radioButtons("typeInput", "Select you beverage type.",
+        choices = unique(bcl$Type), # Notice we can use R code in the arguments
+          #c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+        selected = "WINE"
+      )
+    ),
     mainPanel(
       plotOutput("price_hist"),
       tableOutput("bcl_data")
@@ -19,7 +26,23 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  output$price_hist <- renderPlot(ggplot2::qplot(bcl$Price))
-  output$bcl_data <- renderTable(bcl)
+  #observe(print(input$typeInput))
+  bcl_filtered <- reactive({
+    bcl %>%
+      filter(
+        Price < input$priceInput[2],
+        Price > input$priceInput[1],
+        Type == input$typeInput
+      )
+  })
+
+  output$price_hist <- renderPlot({
+    bcl_filtered() %>%
+      ggplot(aes(Price)) +
+        geom_histogram(bins = 30)
+  })
+  output$bcl_data <- renderTable({
+    bcl_filtered()
+  })
 }
 shinyApp(ui = ui, server = server)
---
+++ cm108/README.md
@@ -0,0 +1,3 @@
+# Shiny Apps Part II
+
+Today we're just continuing from last time. Please see the folder for last week's meeting for code from this lecture.
